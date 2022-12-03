873fc417c922516b2b5f2d7cc90c0aa13e9a24f8
yosefkahlon <yosefkahlon53@gmail.com>
2022-12-03 14:58:38 +0200

    Update inference.py

--- project/inference.py
+++ project/inference.py
@@ -27,12 +27,23 @@ class_mapping = [
     "surprised"
 ]
 
+"""
+model.eval() is a kind of switch for some specific layers/parts of the model that behave differently
+ during training and inference (evaluating) time. For example, 
+ Dropouts Layers, BatchNorm Layers etc. You need to turn off them d
+ uring model evaluation, and .eval() will do it for you. In addition, 
+ the common practice for evaluating/validation is using torch.no_grad() 
+in pair with model.eval() to turn off gradients computation:"""
+
 
 def predict(model, input, target, class_mapping):
-    model.eval()
+    model.eval()  #
     with torch.no_grad():
         predictions = model(input)
-        # Tensor (1, 10) -> [ [0.1, 0.01, ..., 0.6] ]
+        # Tensor (the number of sample that we are passing to the model = 1, the number of classes that the modle tries to predict = 10)
+        # Tensor ( 1, 10) -> [ [0.1, 0.01, ..., 0.6] ]
+        # We are interested in the index that has the highest value.
+        # So that index will correspond to the class that we want to predict.
         predicted_index = predictions[0].argmax(0)
         if predicted_index == 8:
             predicted_index = 7
@@ -43,9 +54,11 @@ def predict(model, input, target, class_mapping):
 
 
 if __name__ == "__main__":
+
     # load back the model
     cnn = CNNNetwork()
-    # cnn = cnn.to('cuda')
+
+    # load the file from the train section
     state_dict = torch.load("feedforwardnet.pth")
     cnn.load_state_dict(state_dict)
 
@@ -72,26 +85,22 @@ if __name__ == "__main__":
                        "cpu")
 
     # get a sample from the dataset for inference
-    print("---------result------------")
+    input = usd[0][0]
+    target = usd[0][1]  # [batch size, num_channels, fr, time]
+    input.unsqueeze_(0)
+
+    # make an inference
+    predicted, expected = predict(cnn, input, target,
+                                  class_mapping)
+    print(f"Predicted :  '{predicted}', expected: '{expected}'")
+
+    # for i in range(50):
+    #     input = usd[i][0]
+    #     target = usd[i][1]  # [batch size, num_channels, fr, time]
+    #     input.unsqueeze_(0)
     #
-    # input = usd[0][0]
-    # target = usd[0][1]  # [batch size, num_channels, fr, time]
-    # input.unsqueeze_(0)
-    #
-    # # make an inference
-    # predicted, expected = predict(cnn, input, target,
-    #                               class_mapping)
-    # print(f"Predicted :  '{predicted}', expected: '{expected}'")
-    # print("\n -------------------------------------------------- \n")
-
-
-    for i in range(50):
-        input = usd[i][0]
-        target = usd[i][1]  # [batch size, num_channels, fr, time]
-        input.unsqueeze_(0)
-
-        # make an inference
-        predicted, expected = predict(cnn, input, target,
-                                      class_mapping)
-        print(f"Predicted {i}:  '{predicted}', expected: '{expected}'")
-        print("\n -------------------------------------------------- \n")
+    #     # make an inference
+    #     predicted, expected = predict(cnn, input, target,
+    #                                   class_mapping)
+    #     print(f"Predicted {i}:  '{predicted}', expected: '{expected}'")
+    #     print("\n -------------------------------------------------- \n")
